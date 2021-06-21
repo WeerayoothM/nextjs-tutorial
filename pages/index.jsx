ed8694272f3673c038a34f9c06efdd9f0c89421b
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-06-21 23:53:06 +0700

    [Added] Structure Pages & Routes

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>HomePage</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem dolores
+        suscipit molestiae sapiente eum vero, sequi delectus nobis deserunt
+        laudantium veniam cupiditate placeat ut, impedit at fugiat obcaecati
+        neque? Illum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem dolores
+        suscipit molestiae sapiente eum vero, sequi delectus nobis deserunt
+        laudantium veniam cupiditate placeat ut, impedit at fugiat obcaecati
+        neque? Illum!
+      </p>
+    </div>
+  );
+}
