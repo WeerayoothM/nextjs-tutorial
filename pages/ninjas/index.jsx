bad3c371a1baa6a7b41bcbfa5513583538e47ff1
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-06-24 13:07:42 +0700

    [Added] Fetching Data (getStaticProps

--- pages/ninjas/index.jsx
+++ pages/ninjas/index.jsx
@@ -1,6 +1,16 @@
 import Head from "next/Head";
+import styles from "../../styles/Ninjas.module.css";
 
-const Ninjas = () => {
+// this function runs at build time as our app is built and our components rendered
+// don't white the code that expect to run in the browser here
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  return { props: { ninjas: data } };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <>
       <Head>
@@ -9,6 +19,13 @@ const Ninjas = () => {
       </Head>
       <div>
         <h1>All Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
