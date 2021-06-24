ab35d8ede025ac3a773a8fd80540d816a5c6e0d2
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-06-24 11:55:48 +0700

    [Added] Adding Styles/ module.css need to use class selectors not element selectors

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,27 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1 className={styles.title}>HomePage</h1>
 
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem dolores
         suscipit molestiae sapiente eum vero, sequi delectus nobis deserunt
         laudantium veniam cupiditate placeat ut, impedit at fugiat obcaecati
         neque? Illum!
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem dolores
         suscipit molestiae sapiente eum vero, sequi delectus nobis deserunt
         laudantium veniam cupiditate placeat ut, impedit at fugiat obcaecati
         neque? Illum!
       </p>
       <Link href="/ninjas">
-        <a>See Ninjs Listing</a>
+        <a className={styles.btn}>See Ninjs Listing</a>
       </Link>
     </div>
   );
